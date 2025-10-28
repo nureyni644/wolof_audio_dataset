66d89628bac801f4114b96aaf451075f80479878
sowelhadji675 <sowelhadji675@gmail.com>
2025-10-28 16:35:44 +0000

    Add S3 integration and audio processing tasks to the DAG
    - Implemented read_from_s3 function to list files in S3 bucket.
    - Added save_to_s3 function to upload processed audio files to S3.
    - Updated download_all_videos to return output files for further processing.
    - Modified DAG to include new tasks for reading from S3 and saving to S3.
    - Updated requirements.txt to include necessary packages for AWS integration.

--- dags/wolof.py
+++ dags/wolof.py
@@ -6,14 +6,36 @@ import yt_dlp
 import subprocess
 import os
 
-# Dossier où seront stockés les WAV
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+BUCKET_NAME = "osmanawsbucket"
+FILE_NAME = "wolof.txt"
+def read_from_s3(**context):
+    ti = context['ti']
+    output_files = ti.xcom_pull(task_ids='download_all_wolof_audio')
+    # print(f"========== {output_files}=======")
+    print("📥 Lecture du fichier depuis S3...")
+    hook = S3Hook(aws_conn_id="aws_default")
+    # buckets = hook.get_bucket(BUCKET_NAME)
+    # print(f"object hook S3: {dir(hook)}")
+    try:
+        keys = hook.list_keys(bucket_name=BUCKET_NAME)
+        if keys:
+            print("✅ Fichiers trouvés dans le bucket :", keys)
+        else:
+            print("🟡 Aucun fichier trouvé dans le bucket (mais accès OK).")
+    except Exception as e:
+        print("❌ Erreur lors du listage :", e)
+
 OUTPUT_DIR = "./data/wavs"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 # Liste de vidéos YouTube wolof à récupérer
 YOUTUBE_URLS = [
-    "https://youtu.be/QIiwPZP1C7I",
-    "https://youtu.be/sJvYJkR4TBo",
+    # "https://youtu.be/QIiwPZP1C7I",
+    "https://www.youtube.com/shorts/k_3q870N8T8?feature=share"
     # Ajouter d'autres URLs ici
 ]
 
@@ -32,17 +54,45 @@ def download_audio(video_url, output_dir=OUTPUT_DIR):
         ext = info_dict['ext']
         input_file = os.path.join(output_dir, f"{video_id}.{ext}")
         output_file = os.path.join(output_dir, f"{video_id}.wav")
-        # Conversion en WAV 16kHz mono
+    
         subprocess.run([
             "ffmpeg", "-y", "-i", input_file, "-ar", "16000", "-ac", "1", output_file
         ])
-        # Optionnel : supprimer le fichier original mp3/m4a
         os.remove(input_file)
+        
+        return output_file
+
+def save_to_s3(**context):
+    ti = context['ti']
+    files = ti.xcom_pull(task_ids='download_all_wolof_audio')
+
+    if not files:
+        print("Aucun fichier à uploader")
+        return
+
+    hook = S3Hook(aws_conn_id="aws_default")
+
+    for local_file in files:
+        filename = os.path.basename(local_file)
+        print(f"Upload {local_file} vers S3 bucket {BUCKET_NAME} sous le nom {filename}")
+        try:
+            hook.load_file(
+                filename=local_file,        
+                key=filename,               
+                bucket_name=BUCKET_NAME,
+                replace=True          
+            )
+            print(f"{filename} uploadé avec succès")
+        except Exception as e:
+            print(f"Erreur lors de l'upload de {filename} :", e)
 
 def download_all_videos():
+    output_files = []
     for url in YOUTUBE_URLS:
-        download_audio(url)
-
+        output_file = download_audio(url)
+        if output_file:
+            output_files.append(output_file)
+    return output_files
 
 # Définition du DAG
 with DAG(
@@ -55,21 +105,16 @@ with DAG(
 
     task_download_audio = PythonOperator(
         task_id="download_all_wolof_audio",
-        python_callable=download_all_videos
+        python_callable=download_all_videos,
     )
 
-    @task
-    def play_audio_files(file_names):
-        from IPython.display import Audio, display
-        for file_name in file_names:
-            display(Audio(filename=file_name))
-    @task
-    def split_audio(file_name:str):
-        from IPython.display import Audio
-        # read a file wav extension
-        import pandas as pd
-        pd
-        pass
-    task_play_audio = play_audio_files([os.path.join(OUTPUT_DIR, f) for f in os.listdir(OUTPUT_DIR) if f.endswith('.wav')])
-    task_download_audio >> task_play_audio
-        
+    task_save_to_s3 = PythonOperator(
+        task_id = "save_to_s3",
+        python_callable = save_to_s3
+    )
+
+    read_task = PythonOperator(
+        task_id="read_from_s3",
+        python_callable=read_from_s3
+    )
+    task_download_audio >> task_save_to_s3>>read_task
